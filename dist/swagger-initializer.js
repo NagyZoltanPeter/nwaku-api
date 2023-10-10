8db91977d67957f4fcf26aa8084796e876afcd8a
NagyZoltanPeter <113987313+NagyZoltanPeter@users.noreply.github.com>
2023-10-10 06:05:54 +0200

    Added reference for nwaku openapi.yaml

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://github.com/waku-org/nwaku/blob/aa1f0f1ed2d498612dfc0328711d9c14a998fc45/waku/waku_api/rest/doc/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
